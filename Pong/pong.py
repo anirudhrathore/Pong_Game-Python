1312d0834679511c87bb3096b9d1518463c861b6
anirudhrathore <rahore.anirudha@gmail.com>
2019-12-25 21:41:44 +0530

    Added the paddle_a, paddle_b and the ball

--- Pong/pong.py
+++ Pong/pong.py
@@ -11,12 +11,29 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0) #speed of animation
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-pad
+paddle_a.goto(-350, 0)
 
 # Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) #speed of animation
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0) #speed of animation
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Function
+
 
 # Main Game Loop
 while True:
